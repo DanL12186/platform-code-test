823b269026e8c5c9883dd6cadb51f29480e6ba5a
DanL12186 <DanL12186@users.noreply.github.com>
2020-07-21 21:47:31 -0500

    initial commit; minor refactor, get tests working.

--- update_quality.rb
+++ update_quality.rb
@@ -1,43 +1,39 @@
-require 'award'
+require 'models/award'
 
 def update_quality(awards)
   awards.each do |award|
+    next if award.name == 'Blue Distinction Plus'
+
+    award.expires_in -= 1
+
+    next if award.quality.zero? || award.quality == 50
+
     if award.name != 'Blue First' && award.name != 'Blue Compare'
       if award.quality > 0
-        if award.name != 'Blue Distinction Plus'
-          award.quality -= 1
-        end
+        award.quality -= (award.name == "Blue Star") ? 2 : 1
       end
     else
-      if award.quality < 50
-        award.quality += 1
-        if award.name == 'Blue Compare'
-          if award.expires_in < 11
-            if award.quality < 50
-              award.quality += 1
-            end
-          end
-          if award.expires_in < 6
-            if award.quality < 50
-              award.quality += 1
-            end
-          end
+      award.quality += 1
+      if award.name == 'Blue Compare'
+        if award.expires_in < 10
+          award.quality += 1
+        end
+        if award.expires_in < 5
+          award.quality += 1
         end
       end
     end
-    if award.name != 'Blue Distinction Plus'
-      award.expires_in -= 1
-    end
+
     if award.expires_in < 0
+      award.quality -= 1 if award.name == "Blue Star"
+        
       if award.name != 'Blue First'
         if award.name != 'Blue Compare'
           if award.quality > 0
-            if award.name != 'Blue Distinction Plus'
-              award.quality -= 1
-            end
+            award.quality -= 1
           end
         else
-          award.quality = award.quality - award.quality
+          award.quality = 0
         end
       else
         if award.quality < 50
